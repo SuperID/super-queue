2bf5ed41af7abfc61a174767b77338cc0829e59c
Zongmin Lei <leizongmin@gmail.com>
2016-10-11 14:36:47 +0800

    使用eslint格式化代码

--- tasks.run.js
+++ tasks.run.js
@@ -9,6 +9,11 @@ const NODE_6 = '6.7.0';
 
 env.DEBUG = 'super-queue:*';
 
+function lint() {
+  console.log(color.green(`执行 eslint 格式化代码...`));
+  exec(`eslint lib --fix`);
+}
+
 function test4() {
   console.log(color.green(`在 Node v${ NODE_4 } 下测试...`));
   exec(`n use ${ NODE_4 } ${ MOCHA } -t 10000`);
@@ -25,7 +30,10 @@ function test6() {
   }
 }
 
-register('test', function () {
+register('lint', '格式化代码', lint);
+
+register('test', '单元测试', function () {
+  lint();
   test4();
   if ($ret !== 0) exit($ret);
   test6();
